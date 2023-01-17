5f45e86c4f644aa66cc9122104a03586bdde75a6
Jesus Arellano Ambrocio <aajesus@live.com.mx>
2023-01-17 15:05:16 -0600

    Ok!

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(sumaTres(10,20,30));
+        sumaTres(10,20,30);
     }
     public static  int sumaTres(int a,int b, int c){
         return a+b+c;

5eb039be5620bcf39af163e331c54a0c906546fa
Jesus Arellano Ambrocio <aajesus@live.com.mx>
2023-01-17 14:52:09 -0600

    First commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(sumaTres(10,20,30));
+    }
+    public static  int sumaTres(int a,int b, int c){
+        return a+b+c;
+    }
+}
